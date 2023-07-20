990e983e299709a6d71cc339cdf41798f3688490
Dingan Mkhize <dingan.mkhize@yahoo.com>
2023-07-20 14:30:16 +0100

    sign up page added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Dashboard from "./components/Dashboard";
 import Exercises from "./components/Exercises";
 import Edit from "./components/Edit";
 import Login from "./components/Login";
+import Signup from "./components/Signup";
 import Footer from "./components/Footer";
 
 import { Routes, Route } from "react-router-dom";
@@ -19,6 +20,7 @@ function App() {
         <Route path="exercises" element={<Exercises />} />
         <Route path="edit" element={<Edit />} />
         <Route path="Login" element={<Login />} />
+        <Route path="Signup" element={<Signup />} />
       </Routes>
       <Footer />
     </>
---
+++ src/components/Signup.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import signupImg from '../assets/signupImg.jpg'
+
+const Signup = () => {
+  return (
+    <div className="h-screen w-full grid grid-cols-1 sm:grid-cols-2">
+      <div className="hidden sm:block">
+        <img className="w-full h-full object-cover" src={signupImg} alt="" />
+      </div>
+
+      <div className="flex flex-col justify-center">
+        <form className="max-w-[400px] w-full mx-auto rounded-md shadow-xl bg-zinc-300 p-6">
+          <h2 className="text-4xl font-bold text-center py-6">
+            Moove & Groove
+          </h2>
+          <div className="flex flex-col py-2">
+            <label>First Name</label>
+            <input className="border p-2" type="text" />
+          </div>
+          <div className="flex flex-col py-2">
+            <label>Surname</label>
+            <input className="border p-2" type="text" />
+          </div>
+          <div className="flex flex-col py-2">
+            <label>Email</label>
+            <input className="border p-2" type="email" />
+          </div>
+          <div className="flex flex-col py-2">
+            <label>Confirm Email</label>
+            <input className="border p-2" type="email" />
+          </div>
+          <div className="flex flex-col py-2">
+            <label>Password</label>
+            <input className="border p-2" type="password" />
+          </div>
+          <button className="border w-full my-3 py-2 bg-indigo-600 rounded-full text-white hover:animate-bounce shadow-x">
+            Sign Up
+          </button>
+          
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Signup
